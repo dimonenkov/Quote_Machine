1cdb106844b409a9f754d534522f7d20eb3a02d3
dimonenkov <dimo.nenkov@me.com>
2018-04-19 00:51:21 +0300

    First Success

--- javascript.js
+++ javascript.js
@@ -1,32 +1,43 @@
-var quotes = ["Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-    "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, " +
-    "when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
-"", "It was popularised in the 1960s with the release of Letraset sheets containing " +
-    "Lorem Ipsum passages, and more recently with desktop publishing software like Aldus " +
-    "PageMaker including versions of Lorem Ipsum."];
+function randomQuote() {
+    $.ajax({
+        url: "https://api.forismatic.com/api/1.0/?",
+        dataType: "jsonp",
+        data: "method=getQuote&format=jsonp&lang=en&jsonp=?",
+        success: function( response ) {
+            $("#random_quote").html("<p id='random_quote' class='lead text-center'>" +
+                response.quoteText + "<br/>&dash; " + response.quoteAuthor + " &dash;</p>");
+            $("#tweet").attr("href", "https://twitter.com/home/?status=" + response.quoteText +
+                ' (' + response.quoteAuthor + ')');
+        }
+    });
+}
 
+$(function() {
+    randomQuote();
+});
+
+$("button").click(function(){
+    randomQuote();
+});
+/*
+//Quotes Changer
+var quotes = [1, 2, 3];
 function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
-    document.getElementById("quoteDisplay").innerHTML = quotes[randomNumber];
+var randomNumber = Math.floor(Math.random() * (quotes.length));
+document.getElementById("quoteDisplay").innerHTML = quotes[randomNumber];
 
 }
-/*
-$(window).load(function(){
-    $( "#change_background" ).on( "click", function() {
-        $("body").first().css("background-color","pink");
-        $("body").second().css("background-color","blue");
-    });
-});
 */
+//Color of Background Changer
 var buton=document.getElementById("buton");
 var allchar="0123456789ABCDEF";
 
 buton.addEventListener("click",myFun);
 
 function myFun(){
-    var  randcol= "";
-    for(var i=0; i<6; i++){
-        randcol += allchar[Math.floor(Math.random()*16)];
-    }
-    document.body.style.backgroundColor= "#"+randcol;
+var  randcol= "";
+for(var i=0; i<6; i++){
+    randcol += allchar[Math.floor(Math.random()*16)];
+}
+document.body.style.backgroundColor= "#"+randcol;
 }
